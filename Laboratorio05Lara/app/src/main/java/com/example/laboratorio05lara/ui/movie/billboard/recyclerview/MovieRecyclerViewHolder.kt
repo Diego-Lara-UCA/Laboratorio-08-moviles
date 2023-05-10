c236d256f39bd8cd4795d89b9747276d69f07e21
Diego-Lara-UCA <00138921@uca.edu.sv>
2023-05-10 11:07:18 -0600

    To do labo 08

--- Laboratorio05Lara/app/src/main/java/com/example/laboratorio05lara/ui/movie/billboard/recyclerview/MovieRecyclerViewHolder.kt
+++ Laboratorio05Lara/app/src/main/java/com/example/laboratorio05lara/ui/movie/billboard/recyclerview/MovieRecyclerViewHolder.kt
@@ -1,5 +1,16 @@
 package com.example.laboratorio05lara.ui.movie.billboard.recyclerview
 
-class MovieRecyclerViewHolder {
-    
+import androidx.recyclerview.widget.RecyclerView
+import com.example.laboratorio05lara.data.ViewModels.MovieModel
+import com.example.laboratorio05lara.databinding.MovieItemBinding
+
+class MovieRecyclerViewHolder (private val binding: MovieItemBinding): RecyclerView.ViewHolder(binding.root){
+    fun bind(movie: MovieModel, clickListener: (MovieModel) -> Unit){
+        binding.titleTextView.text = movie.name
+        binding.qualificationTextView.text = movie.qualification
+
+        binding.movie1Card.setOnClickListener{
+            clickListener(movie)
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Laboratorio-08-moviles
+# To do Laboratorio 08
+
+1- Cargar eficientemente los elementos y permite mostrar de manera escalable una lista de elementos en pantalla. Además permite la personalización , agregar animación e interacutar con dichos elementos.
+
+2-  Elementos del Recycler View:
+    
+    List Item: Define la vista que se mostrará en la lista.
+
+    ViewHolder: se utiliza para almacenar una referencia a la vista de cada elemento de la lista.
+    
+    Adapter: se encarga de vincular los datos de una lista o de un conjunto dedatos con las vistas que se muestran en pantalla.
