ada4ff89f71f3182451a3f8fa04e8a84db5e495e
Diego-Lara-UCA <00138921@uca.edu.sv>
2023-05-10 12:34:44 -0600

    Labo 08 final

--- Laboratorio05Lara/app/src/main/java/com/example/laboratorio05lara/ui/movie/MovieFragment.kt
+++ Laboratorio05Lara/app/src/main/java/com/example/laboratorio05lara/ui/movie/MovieFragment.kt
@@ -5,14 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.laboratorio05lara.R
+import com.example.laboratorio05lara.databinding.FragmentMovieBinding
+import com.example.laboratorio05lara.ui.movie.viewmodel.MovieViewModel
 
 class MovieFragment : Fragment() {
+
+    private val viewModel: MovieViewModel by activityViewModels{
+        MovieViewModel.Factory
+    }
+
+    private lateinit var binding: FragmentMovieBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie, container, false)
+        binding = FragmentMovieBinding.inflate(inflater,container,false)
+        return binding.root
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setViewModel()
+    }
+    private fun setViewModel(){
+        binding.movieviewmodel = viewModel
+    }
+
 }
